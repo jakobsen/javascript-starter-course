a80005a45f117e8865a5dfe003616b93e6071287
Erik André Jakobsen <erik.a.jakobsen@gmail.com>
2019-07-05 20:39:35 +0200

    first commit

---
+++ JS-Sandbox/app.js
@@ -0,0 +1,20 @@
+// What is the Window object?
+// Methods, objects and properties
+
+// Alert
+// alert("Hello World");
+
+// Prompt
+// const input = prompt();
+// alert(input);
+
+// Confirm
+// if (confirm("Are you sure?")) {
+//   console.log("Yes");
+// } else {
+//   console.log("No");
+// }
+
+let val;
+
+console.log(val);
