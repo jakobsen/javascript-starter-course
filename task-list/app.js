8da69d7dbeaaed03f0ae6f140f21ed628111eab3
Erik André Jakobsen <erik.a.jakobsen@gmail.com>
2019-07-11 22:25:39 +0200

    Finished task list UI

--- task-list/app.js
+++ task-list/app.js
@@ -10,26 +10,66 @@ loadEventListeners();
 
 function loadEventListeners() {
   form.addEventListener("submit", addTask);
+
+  // Remove task events
+  taskList.addEventListener("click", removeTask);
+  clearBtn.addEventListener("click", clearAllTasks);
+
+  filter.addEventListener("keyup", filterTasks);
 }
 
 function addTask(e) {
   if (taskInput.value == "") {
     alert("Add a task");
+  } else {
+    // Create li element
+    const li = document.createElement("li");
+    li.className = "collection-item";
+    li.appendChild(document.createTextNode(taskInput.value));
+    const link = document.createElement("a");
+    link.className = "delete-item secondary-content";
+    link.innerHTML = '<i class="fas fa-times"></i>';
+    li.appendChild(link);
+
+    console.log(li);
+
+    taskList.appendChild(li);
+
+    taskInput.value = "";
+    e.preventDefault();
   }
+}
 
-  // Create li element
-  const li = document.createElement("li");
-  li.className = "collection-item";
-  li.appendChild(document.createTextNode(taskInput.value));
-  const link = document.createElement("a");
-  link.className = "delete-item secondary-content";
-  link.innerHTML = '<i class="fas fa-times"></i>';
-  li.appendChild(link);
+// Remove task
+function removeTask(e) {
+  if (e.target.parentElement.classList.contains("delete-item")) {
+    if (confirm("Are you sure?")) {
+      e.target.parentElement.parentElement.remove();
+    }
+  }
+}
 
-  console.log(li);
+function clearAllTasks(e) {
+  // taskList.innerHTML = "";
 
-  taskList.appendChild(li);
+  // Faster
+  while (taskList.firstChild) {
+    taskList.firstChild.remove();
+  }
+}
 
-  taskInput.value = "";
-  e.preventDefault();
+function filterTasks(e) {
+  const text = e.target.value.toLowerCase();
+
+  document -
+    document.querySelectorAll(".collection-item").forEach(function(task) {
+      const item = task.firstChild.textContent;
+      if (item.toLowerCase().indexOf(text) != -1) {
+        task.style.display = "block";
+      } else {
+        task.style.display = "none";
+      }
+    });
+
+  console.log(text);
 }
