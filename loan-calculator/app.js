4598d55fd9b9394669c85d3a692c7c5809fb5ea6
Erik André Jakobsen <erik.a.jakobsen@gmail.com>
2019-07-19 13:46:49 +0200

    Added loading screen

--- loan-calculator/app.js
+++ loan-calculator/app.js
@@ -1,11 +1,19 @@
 // Listen for submit
 
-document
-  .getElementById("loan-form")
-  .addEventListener("submit", calculateResults);
+document.getElementById("loan-form").addEventListener("submit", function(e) {
+  // Hide results
+  document.getElementById("results").style.display = "none";
+
+  // Show loader
+  document.getElementById("loading").style.display = "block";
+
+  setTimeout(calculateResults, 2000);
+
+  e.preventDefault();
+});
 
 // Calculate results
-function calculateResults(e) {
+function calculateResults() {
   console.log("Calculating ...");
   // UI Vars
   const amount = document.getElementById("amount");
@@ -27,11 +35,11 @@ function calculateResults(e) {
     monthlyPayment.value = monthly.toFixed(2);
     totalPayment.value = (monthly * calculatedPayments).toFixed(2);
     totalInterest.value = (monthly * calculatedPayments - principal).toFixed(2);
+    document.getElementById("results").style.display = "block";
   } else {
     showError("Please check your numbers");
   }
-
-  e.preventDefault();
+  document.getElementById("loading").style.display = "none";
 }
 
 // Show error function
