bccea65011835c85c6925a9c8b3aeb5b392e95c8
Erik André Jakobsen <erik.a.jakobsen@gmail.com>
2019-10-24 16:53:01 +0200

    Worked with ES6 classes

--- section-5/app.js
+++ section-5/app.js
@@ -110,42 +110,120 @@
 // console.log(mary.hasOwnProperty("firstName"));
 // console.log(mary.hasOwnProperty("getFullName"));
 
-// **** PROTOTYPAL INHERITANCE ****
-function Person(firstName, lastName) {
-    this.firstName = firstName;
-    this.lastName = lastName;
+// // **** PROTOTYPAL INHERITANCE ****
+// function Person(firstName, lastName) {
+//     this.firstName = firstName;
+//     this.lastName = lastName;
+// }
+
+// // Greeting
+// Person.prototype.greeting = function() {
+//     return `Hello there, ${this.firstName} ${this.lastName}`;
+// }
+
+// const person1 = new Person("John", "Doe");
+// // console.log(person1.greeting());
+
+// // Customer constructor
+// function Customer(firstName, lastName, phoneNumber, membership) {
+//     Person.call(this, firstName, lastName);
+
+//     this.phoneNumber = phoneNumber;
+//     this.membership = membership;
+// }
+
+// // Inherit the Person prototype methods
+// Customer.prototype = Object.create(Person.prototype);
+
+// // Make Customer.prototype return customer
+// Customer.prototype.constructor = Customer;
+
+// // Create customer
+// const customer1 = new Customer("Tom", "Smith", "555-555-5555", "Standard")
+
+// console.log(customer1);
+
+// // Customer greeting
+// Customer.prototype.greeting = function() {
+//     return `Hello there ${this.firstName} ${this.lastName}.\nWelcome to our company!`
+// }
+
+// console.log(customer1.greeting());
+
+// // **** OBJECT CREATE ****
+// const personPrototypes = {
+//     greeting: function() {
+//         return `Hello there ${this.firstName} ${this.lastName}`;
+//     },
+//     getsMarried: function(newLastName) {
+//         this.lastName = newLastName;
+//     } 
+// }
+
+// const mary = Object.create(personPrototypes);
+// mary.firstName = 'Mary';
+// mary.lastName = 'Williams';
+// mary.age = 30;
+// mary.getsMarried('Thompson');
+
+// console.log(mary.greeting());
+
+// const brad = Object.create(personPrototypes, {
+//     firstName: {value: 'Brad'},
+//     lastName: {value: 'Traversy'},
+//     age: {value: 36}
+// });
+
+// console.log(brad);
+// console.log(brad.greeting());
+
+// **** ES6 Classes ****
+class Person {
+    constructor(firstName, lastName, dateOfBirth) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.birthday = new Date(dateOfBirth);
+    }
+
+    greeting() {
+        return `Hello there ${this.firstName} ${this.lastName} `
+    }
+
+    calculateAge() {
+        const diff = Date.now() - this.birthday.getTime();
+        const ageDate = new Date(diff);
+        return Math.abs(ageDate.getUTCFullYear() - 1970);
+    }
+
+    getsMarried(newLastName) {
+        this.lastName = newLastName;
+    }
+
+    // Static methods
+    static addNumbers(x, y) {
+        return x + y;
+    }
 }
 
-// Greeting
-Person.prototype.greeting = function() {
-    return `Hello there, ${this.firstName} ${this.lastName}`;
+const mary = new Person('Mary', 'Thompson', '11/13/1980');
+mary.getsMarried('Smith');
+console.log(mary.greeting());
+console.log(Person.addNumbers(2, 1));
+
+// **** INHERITANCE IN ES6 (Subclasses) ****
+class Customer extends Person {
+    constructor(firstName, lastName, phone, membership) {
+        super(firstName, lastName);
+
+        this.phone = phone;
+        this.membership = membership;
+    }
+
+    static getMembershipCost() {
+        return 500;
+    }
 }
 
-const person1 = new Person("John", "Doe");
-// console.log(person1.greeting());
-
-// Customer constructor
-function Customer(firstName, lastName, phoneNumber, membership) {
-    Person.call(this, firstName, lastName);
-
-    this.phoneNumber = phoneNumber;
-    this.membership = membership;
-}
-
-// Inherit the Person prototype methods
-Customer.prototype = Object.create(Person.prototype);
-
-// Make Customer.prototype return customer
-Customer.prototype.constructor = Customer;
-
-// Create customer
-const customer1 = new Customer("Tom", "Smith", "555-555-5555", "Standard")
-
-console.log(customer1);
-
-// Customer greeting
-Customer.prototype.greeting = function() {
-    return `Hello there ${this.firstName} ${this.lastName}.\nWelcome to our company!`
-}
-
-console.log(customer1.greeting());
+const john = new Customer('John', 'Doe', '555-555-5555', 'Standard');
+console.log(john.greeting());
+console.log(Customer.getMembershipCost());
