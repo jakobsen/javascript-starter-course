6200bfac8b5a0dc5f03bc40d49615a86d53ceec5
Erik André Jakobsen <erik.a.jakobsen@gmail.com>
2019-10-24 19:47:11 +0200

    Add functionality to remove books

--- oop-book-project/app.js
+++ oop-book-project/app.js
@@ -38,6 +38,12 @@ UI.prototype.showAlert = function(message, className) {
   }, 3000);
 }
 
+UI.prototype.deleteBook = function(target) {
+  if (target.className === 'delete') {
+    target.parentElement.parentElement.remove();
+  }
+}
+
 UI.prototype.clearFields = function() {
   document.getElementById('title').value = '';
   document.getElementById('author').value = '';
@@ -63,5 +69,13 @@ document.getElementById('book-form').addEventListener('submit', function(e){
     ui.showAlert('Book added successfully', 'success');
   }
 
+  e.preventDefault();
+})
+
+// Evenet listener for delete
+document.getElementById('book-list').addEventListener('click', function(e) {
+  const ui = new UI;
+  ui.deleteBook(e.target);
+  ui.showAlert('Book removed', 'success');
   e.preventDefault();
 })
