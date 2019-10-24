a0e7fe1827d22c44168d594d1e1f5264a8b12806
Erik André Jakobsen <erik.a.jakobsen@gmail.com>
2019-10-24 18:34:37 +0200

    Started work on JS.
    Users may now add or remove lines to the list, but delete button does not work,
    and users may also submit empty lines. There is no storage, and also no alerts.

---
+++ oop-book-project/app.js
@@ -0,0 +1,43 @@
+// Book Constructor
+function Book(title, author, isbn) {
+  this.title = title;
+  this.author = author;
+  this.isbn = isbn;
+}
+
+
+// UI Constructor
+function UI() {}
+
+UI.prototype.addBookToList = function(book) {
+  const list = document.getElementById('book-list');
+  // Create tr element
+  const row = document.createElement('tr');
+  row.innerHTML = `
+    <td>${book.title}</td>
+    <td>${book.author}</td>
+    <td>${book.isbn}</td>
+    <td><a href="#" class="delete">X</td>
+    `;
+  list.appendChild(row);
+}
+
+UI.prototype.clearFields = function() {
+  document.getElementById('title').value = '';
+  document.getElementById('author').value = '';
+  document.getElementById('isbn').value = '';
+}
+
+// Event listeners
+document.getElementById('book-form').addEventListener('submit', function(e){
+  // Get form values
+  const title = document.getElementById('title').value,
+        author = document.getElementById('author').value,
+        isbn = document.getElementById('isbn').value;
+
+  const book = new Book(title, author, isbn)
+  const ui = new UI();
+  ui.addBookToList(book);
+  ui.clearFields();
+  e.preventDefault();
+})
