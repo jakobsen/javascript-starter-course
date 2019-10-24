eec6000af6af6b8d6473389613907ee8eea6a835
Erik André Jakobsen <erik.a.jakobsen@gmail.com>
2019-10-24 15:20:32 +0200

    Went over constructors for primitives

--- section-5/app.js
+++ section-5/app.js
@@ -17,3 +17,58 @@ function Person(name, dateOfBirth) {
 
 const brad = new Person("Brad", '9/10/1981');
 console.log(brad.calculateAge());
+
+// Built-in Constructors -- not recommended to use!
+//Strings
+const name1 = "Jeff"; // Yes
+const name2 = new String("Jeff"); // No
+
+console.log(name1);
+console.log(name2);
+console.log(name1 == name2);
+console.log(name1 === name2);
+
+// Numbers
+const num1 = 5;
+const num2 = new Number(5);
+
+console.log(num1);
+console.log(num2);
+console.log(num1 == num2);
+console.log(num1 === num2);
+
+// Bools
+const bool1 = true;
+const bool2 = new Boolean(true);
+console.log(bool1 == bool2);
+console.log(bool1 === bool2);
+
+// Function
+const getSum1 = function(x, y) {
+    return x + y
+}
+
+const getSum2 = new Function('x', 'y', 'return x + y');
+
+console.log(getSum1(1, 1));
+console.log(getSum2(1, 1));
+
+// Object
+const john1 = {name: "John"};
+const john2 = new Object({name: "John"});
+console.log(john1);
+console.log(john2);
+
+// Arrays
+const arr1 = [1, 2, 3, 4];
+const arr2 = new Array(1, 2, 3, 4);
+console.log(arr1)
+console.log(arr2)
+console.log(arr1 == arr2) // Would also be false if the two arrays were made the same way
+console.log(arr1 === arr2)
+
+// Regular Expressions
+const re1 = /\w+/;
+const re2 = new RegExp("\\w+");
+console.log(re1);
+console.log(re2);
